de7ed930cb60ec330987e13951381ec0ed70ca73
Ömer Okumuş <omerokumus3@gmail.com>
2023-05-26 09:26:56 +0300

    constructor parameters and properties

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,23 +2,21 @@ import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.colors, {super.key});
+  final List<Color> colors;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
+          colors: colors,
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
       ),
       child: const Center(
-        child: StyledText(),
+        child: StyledText('Hello World!'),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'gradient_container.dart';
 
 void main() {
+  const colors = [
+    Color.fromARGB(255, 26, 2, 80),
+    Color.fromARGB(255, 45, 7, 98),
+  ];
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(colors),
       ),
     ),
   );
